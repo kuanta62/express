ab6f6348e646210e5f5531609c76f08102779def
kuanta62 <kuanta0602@gmail.com>
2024-06-06 23:05:51 +0900

    noteschange

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors'); // corsミドルウェアを追加
-require('dotenv').config();
 
 
 // 接続情報を設定
@@ -9,9 +7,6 @@ const { MongoClient } = require("mongodb");
 //const uri = process.env.MONGODB_URI;
 const uri = "mongodb+srv://test:Kuanta080308200602@cluster0.wn0ljlm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri);
-
-// corsミドルウェアを使用
-router.use(cors());
 
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
